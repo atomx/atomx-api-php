a77675148f9c938748dd03a5f55235ada12bc490
Matthijs Meulenbrug <matthijs@meulenbrug.nl>
2015-04-02 14:16:24 +0200

    Advertiser functions setBudget, etc

--- Atomx/Resources/Advertiser.php
+++ Atomx/Resources/Advertiser.php
@@ -1,5 +1,30 @@
 <?php namespace Atomx\Resources;
 
-class Advertiser {
+use Atomx\AtomxClient;
+use InvalidArgumentException;
 
+class Advertiser extends AtomxClient {
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function setBudget($budget)
+    {
+        $budget = intval($budget);
+
+        if ($budget <= 0)
+            throw new InvalidArgumentException('API: Trying to set an advertiser budget to unlimited (0)');
+
+        $this->budget = 0;
+    }
+
+    public function setBudgetCapping($capping, $pacing = 'ASAP')
+    {
+        if (!in_array($pacing, ['ASAP', 'EVEN']))
+            throw new InvalidArgumentException('API: Invalid pacing provided: ' . $pacing);
+
+        $this->budget_cap_amount = $capping;
+        $this->budget_cap_pacing = $pacing;
+    }
 }
